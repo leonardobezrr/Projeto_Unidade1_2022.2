66f3f3c08ff8ebb522b051a412255b9154c5e03b
Leonardo Bezerra <leonardobezerra05@gmail.com>
2022-11-01 21:30:10 -0300

    corrigindo erros e atualizando struct

--- modulo_usuario.h
+++ modulo_usuario.h
@@ -3,13 +3,8 @@
 #include <locale.h>
 #include <string.h>
 #include "modulo_validacao.h"
-//mod usuário
-void mod_usuario(void);
-void mod_us_cadastrar(void);
-void mod_us_atualizar(void);
-void mod_us_remover(void);
-void mod_us_listar(void);
-void mod_us_procurar(void);
+
+typedef struct usuario Usuario;
 
 struct usuario {
   char nome[31];
@@ -17,6 +12,16 @@ struct usuario {
   char email[41];
   char numero[21];
 };
+//mod usuário
+void mod_usuario(void);
+void mod_us_cadastrar(void);
+void mod_us_atualizar(void);
+void mod_us_remover(void);
+void mod_us_listar();
+void mod_us_procurar(void);
+void exibe_lista(Usuario*);
+
+
 
 //
 // Módulo USUARIO
@@ -58,7 +63,7 @@ void mod_usuario(void){
 }
 ////cadastrar usuário_1//
 void mod_us_cadastrar(void){
-    struct usuario primeiro;
+    Usuario* primeiro;
     setlocale(LC_ALL,"Portuguese");
     printf("\n");
     system("cls||clear");
@@ -69,25 +74,27 @@ void mod_us_cadastrar(void){
     printf("///                           Cadastrar Usuario                             ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");   
+    primeiro = (Usuario*) malloc(sizeof(Usuario));
     printf("Informe seu nome: ");
-    scanf ("%30[A-Z a-z]",primeiro.nome);
+    scanf ("%30[^\n]",primeiro->nome);
     getchar();
     printf("Sua data de nascimento (dd/mm/aa): ");
-    scanf ("%20[0-9/]",primeiro.data);
+    scanf ("%20[^\n]",primeiro->data);
     getchar();
     printf("Email: ");
-    scanf("%40[@_.a-z0-9]",primeiro.email);
+    scanf("%40[^\n]",primeiro->email);
     getchar();
     printf("Telefone (99) 99999-9999: ");
-    scanf ("%20[() 0-9]",primeiro.numero);
+    scanf ("%20[^\n]",primeiro->numero);
     getchar();
+    free(primeiro);
     printf("\n               Cadastrado com sucesso!                                       \n");
     printf("\n///////////////////////////////////////////////////////////////////////////////\n");
     getchar();
 }
 ////atualizar usuario_2///
 void mod_us_atualizar(void){
-    struct usuario primeiro;
+    Usuario* primeiro;
     setlocale(LC_ALL,"Portuguese");
     printf("\n");
     system("cls||clear");
@@ -97,19 +104,21 @@ void mod_us_atualizar(void){
     printf("///                                                                         ///\n");
     printf("///                            Atualizar Usuario                            ///\n");
     printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");   
+    printf("///////////////////////////////////////////////////////////////////////////////\n");  
+    primeiro = (Usuario*) malloc(sizeof(Usuario)); 
     printf("Informe o email: ");
-    scanf("%[@_.a-z0-9]",primeiro.email);
+    scanf("%[@_.a-z0-9]",primeiro->email);
     getchar();
     printf("Informe seu nome: ");
-    scanf ("%[A-Z a-z]",primeiro.nome);
+    scanf ("%[A-Z a-z]",primeiro->nome);
     getchar();
     printf("Sua data de nascimento (dd/mm/aa): ");
-    scanf ("%[0-9/]",primeiro.data);
+    scanf ("%[0-9/]",primeiro->data);
     getchar();
     printf("Telefone (99) 99999-9999: ");
-    scanf ("%[() 0-9]",primeiro.numero);
+    scanf ("%[() 0-9]",primeiro->numero);
     getchar();
+    free(primeiro);
     printf("\n               Atualizado com sucesso!                                       \n");    
     printf("\n///////////////////////////////////////////////////////////////////////////////\n");
     getchar();
@@ -117,7 +126,7 @@ void mod_us_atualizar(void){
 }
 ////remover usuario_3///
 void mod_us_remover(void){
-    struct usuario primeiro;
+    Usuario primeiro;
     setlocale(LC_ALL,"Portuguese");
     printf("\n");
     system("cls||clear");
@@ -137,8 +146,8 @@ void mod_us_remover(void){
 
 }
 ////listar usuario_4///
-void mod_us_listar(void){
-    struct usuario primeiro;
+void mod_us_listar(){
+    Usuario* primeiro;
     setlocale(LC_ALL,"Portuguese");
     printf("\n");
     system("cls||clear");
@@ -149,18 +158,24 @@ void mod_us_listar(void){
     printf("///                             Listar Usuarios                             ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
+    primeiro = (Usuario*) malloc(sizeof(Usuario));
     printf("\nUsuario Cadastrado:\n");
-    printf("Nome: %c\n", primeiro.nome);
-    printf("Email: %c\n", primeiro.email);
-    printf("Data de nascimento: %c\n", primeiro.data);
-    printf("Telefone: %c\n", primeiro.numero);
+    exibe_lista(primeiro);
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     system("pause");
+    free(primeiro);
+}
+/// Exibe lista de usuários//
+void exibe_lista(Usuario* primeiro){
+    printf("Nome: %s\n", primeiro->nome);
+    printf("Email: %s\n", primeiro->email);
+    printf("Data de nascimento: %s\n", primeiro->data);
+    printf("Telefone: %s\n", primeiro->numero);
 }
 ////procurar usuario_5///
 void mod_us_procurar(void){
-    struct usuario primeiro;
+    Usuario* primeiro;
     setlocale(LC_ALL,"Portuguese");
     printf("\n");
     system("cls||clear");
@@ -171,8 +186,9 @@ void mod_us_procurar(void){
     printf("///                             Procurar Usuario                            ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
+    primeiro = (Usuario*) malloc(sizeof(Usuario));
     printf("Informe o email: ");
-    scanf("%[@_.a-z0-9]",primeiro.email);
+    scanf("%[@_.a-z0-9]",primeiro->email);
     getchar();
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
